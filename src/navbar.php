e1286618e5f66f08c5d809b9d955315212d17ce5
Lamine ABDALI <lamine@comelse.com>
2024-09-27 13:04:16 +0200

    .

---
+++ src/navbar.php
@@ -0,0 +1,9 @@
+<div class="header">
+  <a href="localhost:8080" class="logo">Camagru</a>
+  <div class="header-right">
+    <a class="active nav-link" href="#home"><?php echo $translations['home']; ?></a>
+    <a class="nav-link" href="#contact"><?php echo $translations['profile']; ?></a>
+    <a class="nav-link" href="#about"><?php echo $translations['about']; ?></a>
+    <a href="/logout"><img src="logout.png" alt=""></a>
+  </div>
+</div>
